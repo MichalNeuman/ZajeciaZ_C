8ecda369e66a406c3c3d2956eeee51bd5215045e
MichalNeuman <146724475+MichalNeuman@users.noreply.github.com>
2024-02-20 11:57:45 +0100

    Add files via upload

---
+++ zad1.cpp
@@ -0,0 +1,19 @@
+﻿
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+
+int main(){
+    char  tekst[100]; 
+
+    
+    printf("Wprowadź tekst: ");
+
+    
+    scanf("%s", tekst);
+
+    
+    printf("Czesc %s\n", tekst);
+
+    return 0;
+}
