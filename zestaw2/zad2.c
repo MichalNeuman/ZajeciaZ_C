ec710baca9fe1c2a80f73631a97b1150d928e3ee
MichalNeuman <146724475+MichalNeuman@users.noreply.github.com>
2024-02-27 12:45:57 +0100

    Add files via upload

---
+++ zestaw2/zad2.c
@@ -0,0 +1,25 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+void funkcja(float *x) {
+    *x = 2 * *x;
+}
+
+int main() {
+
+    float x = 3.5;
+    printf("%f\n", x);
+
+    funkcja(&x);
+
+    printf("%f\n", x);
+
+
+
+    return 0;
+}// Uruchomienie programu: Ctrl + F5 lub menu Debugowanie > Uruchom bez debugowania
+
+
+
+
+
---
+++ zestaw2/zad3.c
@@ -0,0 +1,41 @@
+﻿#include <stdio.h>
+
+typedef struct{
+    float x;
+    float y;
+}Point;
+
+typedef struct{
+     Point topleft;
+     Point bottomright;
+}Rectangle;
+
+void funcPoint( Point* o) {
+    o->x *= 2;
+    o->y *= 2;
+}
+
+void calculateArea( Rectangle rect) {
+    float width = rect.bottomright.x - rect.topleft.x;
+    float height = rect.topleft.y - rect.bottomright.y;
+    printf("Pole %f\n", width * height);
+    printf("Obwod %f\n", (width + height) * 2);
+    
+}
+
+int main() {
+     Point uno = { 1, 2 };
+    Rectangle rect = { {1, 4}, {5, 1} };
+
+    printf("%f\n", uno.x);
+    printf("%f\n", uno.y);
+
+    funcPoint(&uno);
+
+    printf("%f\n", uno.x);
+    printf("%f\n", uno.y);
+
+    calculateArea(rect);
+
+    return 0;
+}
