27d3423e828760ab801e310c9e8506f51289ec0c
MichalNeuman <146724475+MichalNeuman@users.noreply.github.com>
2024-04-16 10:57:20 +0200

    Create zad2.c

---
+++ zestaw6/zad2.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+    // Tworzenie tablicy dziesięciu liczb całkowitych i inicjalizacja wartościami od 0 do 9
+    int tablica[10];
+    for (int i = 0; i < 10; i++) {
+        tablica[i] = i;
+    }
+    
+    // Używanie wskaźnika do wyświetlenia każdego elementu tablicy z wykorzystaniem arytmetyki wskaźników
+    int *wskaznik = tablica; // Inicjalizacja wskaźnika na pierwszy element tablicy
+    for (int i = 0; i < 10; i++) {
+        printf("Wartość elementu %d: %d\n", i, *wskaznik);
+        wskaznik++; // Inkrementacja wskaźnika, aby przejść do następnego elementu tablicy
+    }
+    
+    return 0;
+}
