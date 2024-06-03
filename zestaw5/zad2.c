69f13746799486902abf38868a7e00494ad653cd
MichalNeuman <146724475+MichalNeuman@users.noreply.github.com>
2024-06-03 17:41:26 +0200

    Update zad2.c

--- zestaw5/zad2.c
+++ zestaw5/zad2.c
@@ -1,49 +1,49 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-// Definicja struktury elementu listy
+
 typedef struct Node {
     int data;
     struct Node* next;
 } Node;
 
-// Funkcja do dodawania elementu na początek listy
+
 void push(Node** head, int value) {
-    // Utwórz nowy element
+    
     Node* new_node = (Node*)malloc(sizeof(Node));
     if (new_node == NULL) {
         printf("Błąd alokacji pamięci!");
         exit(1);
     }
 
-    // Ustaw wartość nowego elementu
+    
     new_node->data = value;
 
-    // Powiąż nowy element, aby wskazywał na obecny początek listy
+    
     new_node->next = *head;
 
-    // Ustaw nowy element jako początek listy
+    
     *head = new_node;
 }
 
-// Funkcja do usuwania pierwszego elementu z listy
+
 void pop(Node** head) {
     if (*head == NULL) {
         printf("Lista jest pusta, nie ma czego usunąć.\n");
         return;
     }
 
-    // Zapamiętaj pierwszy element
+    
     Node* temp = *head;
 
-    // Przesuń głowę listy na następny element
+    
     *head = (*head)->next;
 
-    // Zwolnij pamięć zajmowaną przez pierwotny pierwszy element
+    
     free(temp);
 }
 
-// Funkcja do wyświetlania listy
+
 void display(Node* head) {
     Node* current = head;
     while (current != NULL) {
@@ -53,7 +53,7 @@ void display(Node* head) {
     printf("NULL\n");
 }
 
-// Funkcja do usuwania listy
+
 void delete_list(Node** head) {
     Node* current = *head;
     Node* next;
@@ -66,25 +66,25 @@ void delete_list(Node** head) {
 }
 
 int main() {
-    Node* head = NULL; // Inicjalizacja pustej listy
+    Node* head = NULL; 
 
-    // Dodawanie kilku elementów na początek listy
+    
     push(&head, 3);
     push(&head, 7);
     push(&head, 1);
 
-    // Wyświetlenie listy przed usuwaniem pierwszego elementu
+    
     printf("Lista przed usunięciem pierwszego elementu: ");
     display(head);
 
-    // Usunięcie pierwszego elementu
+    
     pop(&head);
 
-    // Wyświetlenie listy po usunięciu pierwszego elementu
+    
     printf("Lista po usunięciu pierwszego elementu: ");
     display(head);
 
-    // Usunięcie listy (zwolnienie pamięci)
+    
     delete_list(&head);
 
     return 0;
